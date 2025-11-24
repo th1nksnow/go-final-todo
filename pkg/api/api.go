ec043741dcbdd6dcbaa0d037e1f69f305b26906c
th1nksnow <buffallo654@yandex.ru>
2025-11-24 14:28:44 +0300

    init api

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,1 +1,7 @@
 package api
+
+import "net/http"
+
+func Init() {
+	http.HandleFunc("/api/nextdate", NextDateHandler)
+}
---
+++ pkg/api/nextdate.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"net/http"
+	"time"
+)
+
+func NextDate(now time.Time, dstart string, repeat string) (string, error) {
+
+}
+
+func NextDateHandler(w http.ResponseWriter, r *http.Request) {
+
+}
