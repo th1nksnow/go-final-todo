214fa943315e900487e6f83392a4e0fce8f7c158
th1nksnow <buffallo654@yandex.ru>
2025-12-08 02:19:11 +0300

    Enable auth in actions tests & Add token into tests settings

--- tests/settings.go
+++ tests/settings.go
@@ -4,4 +4,4 @@ var Port = 7540
 var DBFile = "../scheduler.db"
 var FullNextDate = true
 var Search = true
-var Token = ``
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzd29yZF9oYXNoIjoiMDNhYzY3NDIxNmYzZTE1Yzc2MWVlMWE1ZTI1NWYwNjc5NTM2MjNjOGIzODhiNDQ1OWUxM2Y5NzhkN2M4NDZmNCIsImV4cCI6MTc2NTE3NzY3NiwiaWF0IjoxNzY1MTQ4ODc2fQ.LmMSIG9g-FsRZ6-7b52TMZdKdQviSSHJ93vN43kvYro`
